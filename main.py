c973b7e499d9629970484554c441351e0513b310
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-04-21 18:22:18 -0400

    Set aspect ratio to equal and enlarge graph

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ from math import sin, cos
 
 
 def main():
-    x_axis, y_axis = zip(*[x_y_of_t(t / 100) for t in range(1, 1000)])
+    # Unpacks list of point tuples into two lists
+    x_axis, y_axis = zip(*[x_y_of_t(t / 1000) for t in range(1, 100000)])
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 10))
+    ax.set_aspect("equal")
     ax.plot(x_axis, y_axis)
 
     plt.show()
 
 
-def x_y_of_t(t):
+def x_y_of_t(t: float) -> tuple:
     return (sin(t) * cos(t) ** 2) / t, (sin(t) ** 2 * cos(t)) / t
 
 
