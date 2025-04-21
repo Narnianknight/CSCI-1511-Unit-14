256e21c8d60c397a81e2ef2466915cb65231dbf0
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-04-21 18:35:11 -0400

    Add title and change style

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ def main():
     # Unpacks list of point tuples into two lists
     x_axis, y_axis = zip(*[x_y_of_t(t / 1000) for t in range(1, 100000)])
 
+    plt.style.use("ggplot")
     fig, ax = plt.subplots(figsize=(15, 10))
     ax.set_aspect("equal")
-    ax.plot(x_axis, y_axis)
+    ax.plot(x_axis, y_axis,linewidth=.5)
+    plt.title("Fancy parametric function")
 
     plt.show()
 
 
 def x_y_of_t(t: float) -> tuple:
+    """
+    Parametric function equivalent to the following function in polar coordinates:
+
+    r = sin(theta)cos(theta) / theta
+
+    Args:
+        t: Parameterization variable
+
+    Returns:
+        tuple: (x, y) tuple
+    """
     return (sin(t) * cos(t) ** 2) / t, (sin(t) ** 2 * cos(t)) / t
 
 
