932f2230df6195defef6f079cb2c9c8d593cfe4c
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-04-21 18:09:11 -0400

    Create plot

---
+++ main.py
@@ -0,0 +1,15 @@
+# Parametric function graph
+# Jonathan Schrock
+# 04/21/2025
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
