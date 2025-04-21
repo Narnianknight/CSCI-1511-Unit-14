3fae33358dd2a9ecd88d5bd62b234d51631ebf7f
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-04-21 18:15:52 -0400

    Create parametric function to plot

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 # 04/21/2025
 
 import matplotlib.pyplot as plt
+from math import sin, cos
 
 
 def main():
-    fig, ax = plt.subplots(figsize=(15, 10))
+    x_axis, y_axis = zip(*[x_y_of_t(t / 100) for t in range(1, 1000)])
+
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, y_axis)
 
     plt.show()
+
+
+def x_y_of_t(t):
+    return (sin(t) * cos(t) ** 2) / t, (sin(t) ** 2 * cos(t)) / t
 
 
 if __name__ == "__main__":
